98b9dfae9b332143a774f293b351f1c4029c74c5
takashi-kubo <takashi.kubo.shootingstar@gmail.com>
2023-07-15 06:28:53 +0900

    initial

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,28 @@
 #include <SoftwareSerial.h>
 #include <TinyGPSPlus.h>
 #include <HardwareSerial.h>
-
 #include <Nextion.h>
 #include <JuncTek_BatteryMonitor.h>
+#include "dischargeCurveForWEM.h"
+#include <SD.h>
 // #define nexSerial uart_DISP
 
 //Display
 //16 is RX_GUI,17 is TX_GUI 
 SoftwareSerial uart_DISP(21, 22); // RX, TX
-NexNumber DispPSpeed = NexNumber(1,11,"x0");    //Pulse Speed
-NexNumber DispGSpeed = NexNumber(1,12,"x1");    //GPS Speed
-NexNumber DispETime = NexNumber(1,13,"x2");     //Elapsed Time
-NexNumber DispVoltage = NexNumber(1,14,"x3");   //Voltage
-NexNumber DispCurrent = NexNumber(1,15,"x4");   //Current
-NexNumber DispPower = NexNumber(1,16,"x5");     //Power
-NexNumber DispIPower = NexNumber(1,17,"x6");    //IntegratedPower
-NexText DispGPSTime = NexText(1,19,"t9");       //GPSTime
+// NexText DispPSpeed = NexText(1,11,"x0");    //Pulse Speed
+NexText DispGSpeed = NexText(1,12,"SPD");    //GPS Speed
+NexText DispETime = NexText(1,13,"ETIME");     //Elapsed Time
+NexText DispVoltage = NexText(1,14,"BV");   //Voltage
+NexText DispCurrent = NexText(1,15,"BA");   //Current
+NexText DispPower = NexText(1,16,"BP");     //Power
+NexText DispICurrent = NexText(1,17,"AH");    //IntegratedCurrent
+NexText DispGPSTime = NexText(1,19,"TIME");       //GPSTime
+NexGauge DispPace = NexGauge(1,2,"PACE");
+NexText DispIM05V = NexText(1,20,"IM05V");
+NexText DispIP05V = NexText(1,21,"IP05V");
+
+
 
 //BatteryMonitor
 //TODO use hardwareserial and change connection
@@ -36,82 +42,165 @@ TinyGPSPlus gps;
 SoftwareSerial uart_VESC(26,27);
 
 //SD
-
-// void getBattMonValue()
-// {
-//   uart_BATT.write(":R50=1,2,1,\r\n"); //Send Command
-//   String _message = "";
-//   char c;
-//   while(uart_BATT.available())  //Wait Data
-//   {
-//     c=uart_BATT.read();         //Receive
-//     _message = _message+String(c);
-//   }
-//   Serial.println(_message);
-
-// }
-
+int fileName = 0;
+char fineNameCHR[16];
+File file;
+char fileHeader[] = {"Time,Etime,Latitude,Longitude,Altitude,Direction,Speed,IdealVoltage,Voltage,Current,Power,DischargeCapacity"};
+bool enableRecord = false;
 
 void setup(){
-    //DebugConsole
-    Serial.begin(115200);
+  //DebugConsole
+  Serial.begin(115200);
 
-    //Display Setup
-    nexSerial.begin(9600);  // Display
+  //Display Setup
+  nexSerial.begin(115200);  // Display
 
-    //BatteryMonitor Setup
-    uart_BATT.setTransmitEnablePin(21);
-    uart_BATT.begin(115200); // BatteryMonitor SerialPort
-    bm.begin(1,uart_BATT);
-    
-    //VESC SetUP
+  //BatteryMonitor Setup
+  uart_BATT.setTransmitEnablePin(13);
+  uart_BATT.begin(115200); // BatteryMonitor SerialPort
+  bm.begin(1,uart_BATT);
+  
+  //VESC SetUP
 
-    //SD Setup
+  //SD Setup
+  if(SD.begin()){
+    for(fileName = 0; fileName < 100;fileName++){
+      sprintf(fineNameCHR,"/%d.csv",fileName);
+      if(!SD.exists(fineNameCHR))break;
+    }
+    Serial.printf("FILE:%s",fineNameCHR);
+    file = SD.open(fineNameCHR,FILE_WRITE);
+    if(file){
+      enableRecord = true;
+      file.println(fileHeader);
+      file.close();
+    }
+    else{
+      Serial.printf("ERROR File Open");
+    }
+  }
+  // else{
+  //   DispPower.setText("NO SD");
+  //   while(1){
 
-    //GPS Setup
-    //Connection 赤色=VCC、黒色=GND、橙色=TXD、緑色=RXD、茶色=1pps
-    uart_GPS.begin(9600,SERIAL_8N1,14,4);
+  //   }
+  // }
+  //GPS Setup
+  //Connection 赤色=VCC、黒色=GND、橙色=TXD、緑色=RXD、茶色=1pps
+  uart_GPS.begin(9600,SERIAL_8N1,14,4);
 }
+
 double BattVoltage;
 double BattCurrent;
+double IntCurrent;
+double VoltageBuf;
+int ElapsedTime = 0;
+char bufTime[16];
+char bufETime[16];
+char bufSpeed[16];
+char bufBattV[16];
+char bufBattA[16];
+char bufBattP[16];
+char bufBattIA[16];
+char bufIM05V[16];
+char bufIP05V[16];
+double im05v,ip05v;
 
 void loop(){
 
-    //GPS
-    char buf[64];
-    bool gpsUpdated = false;
-    while (gpsUpdated == false){
-        while(uart_GPS.available() > 0 ){
-          char c = uart_GPS.read();
-          // Serial.print(c);
-          gps.encode(c);
-          gpsUpdated = gps.time.isUpdated();
-        }
+  long time_Start = millis();
+
+  //GPS
+  bool gpsUpdated = false;
+  while (gpsUpdated == false){
+      while(uart_GPS.available() > 0 ){
+        char c = uart_GPS.read();
+        // Serial.print(c);
+        gps.encode(c);
+        gpsUpdated = gps.time.isUpdated();
+      }
+  }
+
+  long time_gps = millis();
+
+  //BatteryMonitor
+  uart_BATT.listen();
+
+  VoltageBuf = bm.getVoltage();
+  //115200bpsのSoftwareSerialのため文字化けが頻発する
+  //電圧が3.0以上の時更新する
+  if(VoltageBuf >= 3.0){
+    BattVoltage = bm.getVoltage();
+    BattCurrent = bm.getCurrent();
+  }
+
+  double BattPower = BattVoltage * BattCurrent;
+  IntCurrent+=BattCurrent;
+
+  long time_bm = millis();
+
+  sprintf(bufTime,"%02d:%02d:%02d",(gps.time.hour()+9)%24,gps.time.minute(),gps.time.second());
+  sprintf(bufSpeed, "%2.1f",gps.speed.kmph());
+  sprintf(bufBattV,"%2.1f",BattVoltage);
+  sprintf(bufBattA,"%2.1f",BattCurrent);
+  sprintf(bufBattP,"%3.1f",BattPower);
+  sprintf(bufBattIA,"%2.1f",IntCurrent/3600);
+  sprintf(bufETime,"%02d:%02d:%02d",ElapsedTime/3600,(ElapsedTime/60)%60,ElapsedTime%60);
+  im05v = idealCurve[ElapsedTime]-0.5;
+  ip05v = idealCurve[ElapsedTime]+0.5;
+  sprintf(bufIM05V,"%2.1fV",im05v);
+  sprintf(bufIP05V,"%2.1fV",ip05v);
+  double VoltDiff = BattVoltage - idealCurve[ElapsedTime];
+
+  long time_disp_sprint = millis();
+
+  delay(10);  //delay 10ms for flush uart buffer...
+  //DISPLAY
+  uart_DISP.listen();
+  DispGPSTime.setText(bufTime);
+  DispGSpeed.setText(bufSpeed);
+  DispVoltage.setText(bufBattV);
+  DispCurrent.setText(bufBattA);
+  DispPower.setText(bufBattP);
+  DispICurrent.setText(bufBattIA);
+  DispETime.setText(bufETime);
+  DispIM05V.setText(bufIM05V);
+  DispIP05V.setText(bufIP05V);
+  if(BattVoltage <= im05v){
+      DispPace.setValue(0);
+  }
+  else if(ip05v <= BattVoltage){
+      DispPace.setValue(100);
+  }
+  else{
+      DispPace.setValue((int)((BattVoltage - im05v) * 100));
+  }
+
+  long time_disp = millis();
+
+  //Write to SD
+  if(enableRecord){
+    file = SD.open(fineNameCHR,FILE_APPEND);
+    if(file){
+      char buf[256];
+      sprintf(buf,"%s,%s,%f,%f,%f,%f,%s,%f,%s,%s,%s,%s",
+      bufTime,bufETime,gps.location.lat(),gps.location.lng(),gps.altitude.meters(),gps.course.deg(),bufSpeed,
+      idealCurve[ElapsedTime],bufBattV,bufBattA,bufBattP,bufBattIA);
+      file.println(buf);
+      file.close();
     }
-    sprintf(buf,"%02d:%02d:%02d speed %2.1f, heading to %3f",gps.time.hour()+9,gps.time.minute(),gps.time.second(),gps.speed.kmph(),gps.course.deg());
+  }
 
-    // Serial.print(buf);
-    //BatteryMonitor
-    uart_BATT.listen();
-    // uart_BATT.flush();
-    // getBattMonValue();
+  long time_sd = millis();
 
-    double CurrentBuf = bm.getCurrent();
-    //115200bpsのSoftwareSerialのため文字化けが頻発する
-    //文字化けデータは0が返るので0=無効値として扱う
-    if(CurrentBuf != 0.0){
-      BattVoltage = bm.getVoltage();
-      BattCurrent = bm.getCurrent();
-    }
-    double BattPower = BattVoltage * BattCurrent;
-    DispVoltage.setValue((uint32_t)(floor(BattVoltage*100.0 )));
+  ElapsedTime++;
 
-    DispGPSTime.setText(buf);
-    Serial.printf("V=%2.1f,A=%f,P=%f, %s\n",BattVoltage,BattCurrent,BattPower,buf);
-    // Serial.printf("%s\n",buf);
 
-    // uart_GPS.listen();
-    // delay(500);
+  Serial.printf("GPS:%3ld,BM:%3ld,SPRINT:%3ld,DISP:%3ld,SD:%3ld\n",
+  time_gps - time_Start,time_bm - time_gps,time_disp_sprint - time_bm,time_disp - time_disp_sprint,time_sd - time_disp);
+
+  // uart_GPS.listen();
+  // delay(500);
 }
 
 
